03226966278032582e6eadc74d5c5870d73010f5
semal55 <semal551@gmail.com>
2016-09-02 23:43:12 +0600

    Лаба 1, задание 1, подзадание 1

---
+++ lab_1_1_1/lab_1_1_1.c
@@ -0,0 +1,28 @@
+ #include <stdio.h>
+ #include <conio.h>
+ #include <locale.h>
+ #include <math.h>
+
+ void main()
+{
+
+    double w;
+    double a = 1.5;
+    double b = 2;
+    double c = -0.7;
+    /*
+
+    !!!!Внимание!!!!
+
+    */
+    //Изменяем х в зависимости от задачи. Нужное раскомментировать!
+   double x = 1;
+   // double x = 2;
+
+
+    w = a*exp(-sqrt(x))*cos(b*x)+pow(c,5);
+
+    printf("w = %f",w);
+
+    getche();
+}
