ef819e88cdb6324d28f7213473c5d23ce5225452
semal55 <semal551@gmail.com>
2016-09-03 00:20:26 +0600

    Лаба 1, задание 1, подзадание 3

---
+++ lab_1_1_3/lab_1_1_3.c
@@ -0,0 +1,29 @@
+ #include <stdio.h>
+ #include <conio.h>
+ #include <locale.h>
+ #include <math.h>
+
+ void main()
+{
+
+    double z;
+    double a = 2;
+    double b = 1.2;
+    double c = 1;
+    /*
+
+    !!!!Внимание!!!!
+
+    */
+    //Изменяем х в зависимости от задачи. Нужное раскомментировать!
+
+    //double x = 0;
+    //double x = 2;
+
+
+    z = sqrt(a + b - exp(sin(x)) + c );
+
+    printf("w = %f",z);
+
+    getche();
+}
