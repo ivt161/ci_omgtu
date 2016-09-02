8ca78c5239e51c3331f19234d93b83e39514cced
semal55 <semal551@gmail.com>
2016-09-03 00:14:40 +0600

    Лаба 1, задание 1, подзадание 2

---
+++ lab_1_1_2/lab_1_1_2.c
@@ -0,0 +1,28 @@
+ #include <stdio.h>
+ #include <conio.h>
+ #include <locale.h>
+ #include <math.h>
+
+ void main()
+{
+
+    double y;
+    double a = 0.7;
+    double c = 2.1;
+    /*
+
+    !!!!Внимание!!!!
+
+    */
+    //Изменяем х в зависимости от задачи. Нужное раскомментировать!
+
+   //double x = 1;
+   // double x = 2;
+
+
+    y = sin(x) / sqrt(1 + pow(a,2)* pow(sin(x), 2))- c*log(a*x);
+
+    printf("w = %f",y);
+
+    getche();
+}
