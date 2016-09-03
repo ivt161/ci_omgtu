a8fe466741816e9c78f97fa0f1a3f9ffaa75e916
semal55 <semal551@gmail.com>
2016-09-04 02:04:28 +0600

    lab_1_2_9

---
+++ lab_1_2_9/lab_1_2_9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+void main()
+{
+
+    int number;
+
+    printf("Enter number: \n");
+    scanf("%d",&number);
+
+    printf("number = %o",number);
+
+    getch();
+}
